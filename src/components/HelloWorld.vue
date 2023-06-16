f8e1a6eb7dff8604f57694d9a1df62e481a76847
Gautam Thaker <ghthaker@gmail.com>
2023-06-16 17:14:15 -0400

    update the client-id to one for Test1 app on fhir.epic.com

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -73,7 +73,7 @@ export default {
       accesstoken: "",
       patient: "",
       patientdata: {},
-      clientId: "42f3b173-16a8-4c50-a3ea-0269294cb869", // Replace with your client id
+      clientId: "ac1fc65c-cdcf-481d-b009-5c36c72ad083", // Replace with your client id
       redirect: import.meta.env.PROD
         ? "https://lucid-wozniak-940eae.netlify.app"
         : "http://localhost:3000",
